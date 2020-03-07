cbfbc1ad712422b144b6ffbc582bc5b1d8474970
hidaxphoenix <zeecombu2115@gmail.com>
2020-03-07 14:22:36 +0900

    confirm_action 作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :items do
+    collection {get "confirm"}
+      resources :items, only: [:show]
+  end
 end
