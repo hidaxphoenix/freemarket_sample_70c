8d01e2438c02669bfbd26fc2f4adb7d3c16071ff
nakanishiyasuhiro <60539407+nakanishiyasuhiro@users.noreply.github.com>
2020-03-09 21:41:18 +0900

    user-fix

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -65,7 +65,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def create
     @user = User.new(sign_up_params)
-    p @user
     unless @user.valid?
       flash.now[:alert] = @user.errors.full_messages
       render :new and return
