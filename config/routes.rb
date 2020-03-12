5715ef758e5a7ac3a6c6097a425fe51759f7e0cc
yokki167 <60569769+yokki167@users.noreply.github.com>
2020-03-12 14:47:38 +0900

    fix_function_category

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,13 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :items do
 
-    # collection {get "confirm"} do
-    #   resources :items, only: [:show]
-    # end
+    collection {get "confirm"} 
+      resources :items, only: [:show]
+    
 
     collection do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
-      
     end
 
   end
