b80eba99f90da50ce0ca4d3c8e444e0665ed4556
nakanishiyasuhiro <60539407+nakanishiyasuhiro@users.noreply.github.com>
2020-03-17 15:49:19 +0900

    add_comments_set

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,15 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
-    redirect_to "/items/#{comment.item.id}"
+    if comment = Comment.create(comment_params)
+      redirect_to "/items/#{comment.item.id}", notice:'コメントされました。'
+    else 
+      render :show, notice:'コメントの送信に失敗しました。'
+    end
   end
 
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
   end
+  
 end
