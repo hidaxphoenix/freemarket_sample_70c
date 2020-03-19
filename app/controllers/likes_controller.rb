50ac7ed6445984832a1a703340e0123a9496413c
nakanishiyasuhiro <60539407+nakanishiyasuhiro@users.noreply.github.com>
2020-03-19 11:35:48 +0900

    add_likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,14 +5,12 @@ class LikesController < ApplicationController
   def create
     @like = Like.create(user_id: current_user.id, item_id: params[:item_id])
     @likes = Like.where(item_id: params[:item_id])
-    get_item
   end
   
   def destroy
     @like = Like.find_by(user_id: current_user.id, item_id: params[:item_id])
     @like.destroy
     @likes = Like.where(item_id: params[:item_id])
-    get_item
   end
   
   def get_item
